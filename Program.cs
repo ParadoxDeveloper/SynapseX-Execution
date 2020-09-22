9b941f7104d58343712ee09e53d11c6fbf5763ef
ParadoxDeveloper <68805605+ParadoxDeveloper@users.noreply.github.com>
2020-09-22 10:16:05 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sxlib;
+using sxlib.Specialized;
+
+namespace ScriptExecutorSXLIB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SLib = SxLib.InitializeOffscreen('./Synapse X.exe');
+            SLib.LoadEvent += Ev =>
+            {
+                if (Ev == SxLibBase.SynLoadEvents.READY)
+                {
+                    SLib.Attach();
+                    if (Ev == SxLibBase.SynAttachEvents.READY)
+                    {
+                        SLib.Execute('Code Here');
+
+
+                    }
+
+                }
+            };
+            SLib.Load();
+        }
+    }
+}
